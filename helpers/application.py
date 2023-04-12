776afa8b1576565e066e54e5bdf73a52dcebb695
aafonin <aafonin@mtt.ru>
2023-04-12 17:49:52 +0300

    Initial commit

---
+++ helpers/application.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+class Application:
+    pass
+
+    def __init__(self):
+        self.wd = WebDriver('../webdriver/chromedriver')
+        self.wd.implicitly_wait(60)
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("https://demoqa.com/text-box")
+
+    def find_elements_by_xpath(self, x_path):
+        element = self.wd.find_element("xpath", x_path)
+        return element
+
+    def get_element_text(self, x_path):
+        element_text = self.wd.find_element("xpath", x_path).text
+        return element_text
+
+    def destroy(self):
+        self.wd.quit()
---
+++ tests/new_test.py
@@ -0,0 +1,51 @@
+import time
+
+import pytest
+from selenium.webdriver.common.by import By
+from faker import Faker
+from helpers.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_fill_form(app):
+    fake = Faker()
+
+    app.open_home_page()
+
+    full_name = fake.name()
+    email = fake.email()
+    cur_address = fake.address().replace('\n', '')
+    per_address = fake.address().replace('\n', '')
+
+    field_full_name = app.find_elements_by_xpath("//*[@placeholder='Full Name']")
+    field_email = app.find_elements_by_xpath("//*[@placeholder='name@example.com']")
+    field_cur_address = app.find_elements_by_xpath("//textarea[@id='currentAddress']")
+    field_per_address = app.find_elements_by_xpath("//textarea[@id='permanentAddress']")
+
+    field_full_name.send_keys(full_name)
+    field_email.send_keys(email)
+    field_cur_address.send_keys(cur_address)
+    field_per_address.send_keys(per_address)
+
+    button_submit = app.wd.find_element(By.ID, "submit")
+    button_submit.click()
+
+    text_full_name = app.get_element_text("//p[@id='name']")
+    text_email = app.get_element_text("//p[@id='email']")
+    text_cur_address = app.get_element_text("//p[@id='currentAddress']")
+    text_per_address = app.get_element_text("//p[@id='permanentAddress']")
+
+    # time.sleep(3)  # Let the user actually see something!
+
+    assert text_full_name == ("Name:" + full_name)
+    assert text_email == ("Email:" + email)
+    assert text_cur_address == ("Current Address :" + cur_address)
+    assert text_per_address == ("Permananet Address :" + per_address)
+
+
